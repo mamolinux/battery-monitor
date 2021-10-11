03c9f54dc746f08f63de07384a8844a1794cd7c2
Himadri Sekhar Basu <hsb10@iitbbs.ac.in>
2021-10-11 20:44:50 +0530

    Update repo website

---
+++ styles/jscripts/download.js
@@ -0,0 +1,6 @@
+document.getElementById("demo").innerHTML = "My First JavaScript";
+
+function fun1()
+{
+	document.getElementById("demo1").innerHTML = "document.location='freedom-repo/releases/freedom-20.04.1-LTS-amd64.iso'";
+}
