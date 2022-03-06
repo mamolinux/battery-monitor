93729c83fa84a6962b56eec14b14f98dfd4b22fb
Himadri Sekhar Basu <41947504+hsbasu@users.noreply.github.com>
2022-03-06 10:01:15 +0530

    Add option to enable/disable sound notification; closes #4 (#15)

--- usr/lib/battery-monitor/Notification.py
+++ usr/lib/battery-monitor/Notification.py
@@ -111,6 +111,10 @@ class get_notification():
 			except ValueError:
 				self.critical_battery = 10
 			try:
+				self.use_sound = int(self.config['settings']['use_sound'])
+			except ValueError:
+				self.use_sound = 1
+			try:
 				self.notification_stability = int(self.config['settings']['notification_stability'])
 			except ValueError:
 				self.notification_stability = 5
@@ -123,6 +127,7 @@ class get_notification():
 			self.third_custom_warning = -3
 			self.low_battery = 30
 			self.critical_battery = 10
+			self.use_sound = 1
 			self.notification_stability = 5
 	
 	def show_notification(self, notiftype: str, battery_percentage: int,
@@ -138,7 +143,8 @@ class get_notification():
 				if ("charging" or "discharging") in notiftype:
 					notification = self.notifier.new(head, body, icon)
 					notification.show()
-					os.system("paplay /usr/share/sounds/Yaru/stereo/complete.oga")
+					if self.use_sound:
+						os.system("paplay /usr/share/sounds/Yaru/stereo/complete.oga")
 				else:
 					self.monitor.is_updated()
 					info = self.monitor.get_processed_battery_info()
@@ -147,7 +153,8 @@ class get_notification():
 						continue
 					notification = self.notifier.new(head, body, icon)
 					notification.show()
-					os.system("paplay /usr/share/sounds/Yaru/stereo/complete.oga")
+					if self.use_sound:
+						os.system("paplay /usr/share/sounds/Yaru/stereo/complete.oga")
 					time.sleep(self.notification_stability)
 				
 		except GLib.GError as e:
