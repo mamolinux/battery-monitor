09d06d212beed1c3331f4be7639596cf57496872
Himadri Sekhar Basu <hsb10@iitbbs.ac.in>
2022-04-05 09:15:17 +0530

    Add and update copyright to 2022 and code cleanup
    - AboutWindow.py: remove unused imports
    - AppIndicator.py: remove unused lines

--- usr/lib/battery-monitor/ErrorLib.py
+++ usr/lib/battery-monitor/ErrorLib.py
@@ -1,5 +1,28 @@
 #!/usr/bin/python3
 
+# Copyright: 2016-2020 Maksudur Rahman Maateen <maateen@outlook.com>
+#            2021-2022 Himadri Sekhar Basu <hsb10@iitbbs.ac.in>
+#
+# This file is part of battery-monitor.
+#
+# battery-monitor is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# battery-monitor is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with battery-monitor. If not, see <http://www.gnu.org/licenses/>
+# or write to the Free Software Foundation, Inc., 51 Franklin Street,
+# Fifth Floor, Boston, MA 02110-1301, USA..
+#
+# Author: Maksudur Rahman Maateen <maateen@outlook.com>
+#
+
 class Error(Exception):
     """Base class for exceptions in this module."""
     pass
--- usr/lib/battery-monitor/SettingsWindow.py
+++ usr/lib/battery-monitor/SettingsWindow.py
@@ -1,4 +1,27 @@
-#!/usr/bin/env python3
+#!/usr/bin/python3
+
+# Copyright: 2016-2020 Maksudur Rahman Maateen <maateen@outlook.com>
+#            2021-2022 Himadri Sekhar Basu <hsb10@iitbbs.ac.in>
+#
+# This file is part of battery-monitor.
+#
+# battery-monitor is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# battery-monitor is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with battery-monitor. If not, see <http://www.gnu.org/licenses/>
+# or write to the Free Software Foundation, Inc., 51 Franklin Street,
+# Fifth Floor, Boston, MA 02110-1301, USA..
+#
+# Author: Maksudur Rahman Maateen <maateen@outlook.com>
+#
 
 # standard library
 import configparser
@@ -297,6 +320,3 @@ class SettingsWindow():
 	def on_quit(self, widget):
 		self.window.close()
 	
-	# def __quit(self, *args):
-	# 	Gtk.main_quit()
-		
