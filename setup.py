1afddf2b63a57231cabaaf257b469d7fd3ce269d
Himadri Sekhar Basu <hsb10@iitbbs.ac.in>
2024-03-02 13:13:17 +0530

    Update translation
    - Move translation sources to po
    - Add functions to generate .mo files

--- setup.py
+++ setup.py
@@ -1,5 +1,6 @@
-
 import glob
+import os
+import subprocess
 
 from setuptools import setup
 from subprocess import check_output
@@ -18,10 +19,30 @@ with open("src/BatteryMonitor/VERSION", "w") as f:
         version = version.split('~')[0]
     f.write("%s" % version)
 
-setup(
-    data_files=[('share/battery-monitor/ui', glob.glob("data/ui/*")),
-            ('share/applications', glob.glob("data/applications/*.desktop")),
-            ('share/icons/hicolor/scalable/apps', glob.glob("data/icons/*")),
-            ('share/glib-2.0/schemas', glob.glob("data/*.xml")),
-            ]
+gschema_dir_suffix = 'share/glib-2.0/schemas'
+
+data_files = [('share/applications', glob.glob("data/applications/*.desktop")),
+			('share/icons/hicolor/scalable/apps', glob.glob("data/icons/*")),
+			(gschema_dir_suffix, glob.glob("data/schema/*.xml"))
+			]
+
+def create_mo_files():
+	po_files = glob.glob("po/*.po")
+	prefix = 'battery-monitor'
+	
+	for po_file in po_files:
+		po_name = os.path.splitext(os.path.split(po_file)[1])[0]
+		replace_txt = "%s-" % prefix
+		lang = po_name.replace(replace_txt, '')
+		os.makedirs("data/locale/%s" % lang, exist_ok=True)
+		mo = "data/locale/%s/%s.mo" % (lang, prefix)
+		subprocess.run(['msgfmt', '-o', str(mo), po_file], check=True)
+		
+		mo_file = map(lambda i: ('share/locale/%s/LC_MESSAGES' % lang, [i+'/%s.mo' % prefix]), glob.glob('data/locale/%s' % lang))
+		data_files.extend(mo_file)
+
+create_mo_files()
+
+setup(data_files=data_files,
+		# cmdclass = {'install_data': install_data}
 )
