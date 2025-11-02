319b12af484b71850bbcbd4f8c504a5c42c64a89
Himadri Sekhar Basu <41947504+hsbasu@users.noreply.github.com>
2025-11-02 15:58:38 +0530

    Set process title and update logger (#72)
    - Include line number in logs
    - Check and create log path

--- src/BatteryMonitor/config.py
+++ src/BatteryMonitor/config.py
@@ -70,7 +70,10 @@ def create_logfile():
 		logfile = logpath + APP + dlimitter + username + dlimitter + random_code + '.log'
 	else:
 		logfile = glob.glob(logpath+APP+dlimitter+username+'*')[0]
-
+	
+	if not os.path.exists(logpath):
+		os.makedirs(logpath, exist_ok=True)
+	
 	return logpath, logfile
 
 # Set the log filename
@@ -93,108 +96,108 @@ UI_PATH = _path+"/ui/"
 _icon_path = _path + '/icons/'
 
 ICONS = {
-    "app": "tray-icon.svg",
-    "success": "icon.png",
-    "null": "icon.png",
-    "fail": "icon.png",
-    "acpi": "icon.png",
-    "charging": "charging.png",
-    "discharging": "discharging.png",
-    "full":  "full-charge.png",
-    "unknown":  "not-charging.png",
-    "low_battery": "low-battery.png",
-    "critical_battery": "critical-battery.png",
-    "first_custom_warning": "discharging.png",
-    "second_custom_warning": "discharging.png",
-    "third_custom_warning": "discharging.png",
-    "upper_threshold_warning": "charging.png"
+	"app": "tray-icon.svg",
+	"success": "icon.png",
+	"null": "icon.png",
+	"fail": "icon.png",
+	"acpi": "icon.png",
+	"charging": "charging.png",
+	"discharging": "discharging.png",
+	"full":  "full-charge.png",
+	"unknown":  "not-charging.png",
+	"low_battery": "low-battery.png",
+	"critical_battery": "critical-battery.png",
+	"first_custom_warning": "discharging.png",
+	"second_custom_warning": "discharging.png",
+	"third_custom_warning": "discharging.png",
+	"upper_threshold_warning": "charging.png"
 }
 
 for key in ICONS:
 	ICONS[key] = _icon_path + ICONS[key]
 
 MESSAGES = {
-    "success": (
-        _("Battery Monitor"),
-        _("Cheers! Your battery is being monitored now.")
-    ),
+	"success": (
+		_("Battery Monitor"),
+		_("Cheers! Your battery is being monitored now.")
+	),
 	
 	"null": (
 		(""),
 		("")
 	),
 
-    "fail": (
-        _("Battery Monitor"),
-        _("Alas! Battery is not yet present!")
-    ),
+	"fail": (
+		_("Battery Monitor"),
+		_("Alas! Battery is not yet present!")
+	),
 
-    "acpi": (
-        _("Battery Monitor"),
-        _("Dependency Error! acpi is not installed.")
-    ),
+	"acpi": (
+		_("Battery Monitor"),
+		_("Dependency Error! acpi is not installed.")
+	),
 
-    "charging": (
-        _("Charging"),
-        _("Now {battery_percentage} %, {remaining_time}")
-    ),
+	"charging": (
+		_("Charging"),
+		_("Now {battery_percentage} %, {remaining_time}")
+	),
 
-    "discharging": (
-        _("Discharging"),
-        _("Now {battery_percentage} %, {remaining_time}")
-    ),
+	"discharging": (
+		_("Discharging"),
+		_("Now {battery_percentage} %, {remaining_time}")
+	),
 
-    "full": (
-        _("Fully Charged - Unplug your Charger"),
-        _("{battery_percentage} % Remaining")
-    ),
+	"full": (
+		_("Fully Charged - Unplug your Charger"),
+		_("{battery_percentage} % Remaining")
+	),
 
-    "unknown": (
-        _("Fully Charged"),
-        _("{battery_percentage} % Remaining")
-    ),
+	"unknown": (
+		_("Fully Charged"),
+		_("{battery_percentage} % Remaining")
+	),
 
-    "low_battery": (
-        _("Low Battery"),
-        _("Now {battery_percentage} %, {remaining_time}")
-    ),
+	"low_battery": (
+		_("Low Battery"),
+		_("Now {battery_percentage} %, {remaining_time}")
+	),
 
-    "critical_battery": (
-        _("Critically Low Battery"),
-        _("Only {battery_percentage} %, {remaining_time}")
-    ),
+	"critical_battery": (
+		_("Critically Low Battery"),
+		_("Only {battery_percentage} %, {remaining_time}")
+	),
 
-    "first_custom_warning": (
-        _("First Custom Warning"),
-        _("Now {battery_percentage} %, {remaining_time}")
-    ),
+	"first_custom_warning": (
+		_("First Custom Warning"),
+		_("Now {battery_percentage} %, {remaining_time}")
+	),
 
-    "second_custom_warning": (
-        _("Second Custom Warning"),
-        _("Now {battery_percentage} %, {remaining_time}")
-    ),
+	"second_custom_warning": (
+		_("Second Custom Warning"),
+		_("Now {battery_percentage} %, {remaining_time}")
+	),
 
-    "third_custom_warning": (
-        _("Third Custom Warning"),
-        _("Now {battery_percentage} %, {remaining_time}")
-    ),
-    "upper_threshold_warning": (
-        _("Upper Threshold Warning - Unplug your Charger"),
-        _("Now {battery_percentage} %, {remaining_time}")
-    )
+	"third_custom_warning": (
+		_("Third Custom Warning"),
+		_("Now {battery_percentage} %, {remaining_time}")
+	),
+	"upper_threshold_warning": (
+		_("Upper Threshold Warning - Unplug your Charger"),
+		_("Now {battery_percentage} %, {remaining_time}")
+	)
 }
 
 TEST_CASES = {
-    "state": [
-        _("Full"),
-        _("Charging"),
-        _("Discharging")
-    ],
-    "remaining": [
-        _("00:10:12 remaining"),
-        _("01:47:31 remaining"),
-        _("02:33:47 remaining"),
-        _("03:24:25 remaining"),
-        _("discharging at zero rate - will never fully discharge")
-    ]
+	"state": [
+		_("Full"),
+		_("Charging"),
+		_("Discharging")
+	],
+	"remaining": [
+		_("00:10:12 remaining"),
+		_("01:47:31 remaining"),
+		_("02:33:47 remaining"),
+		_("03:24:25 remaining"),
+		_("discharging at zero rate - will never fully discharge")
+	]
 }
