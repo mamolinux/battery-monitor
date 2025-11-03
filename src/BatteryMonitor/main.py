d591da758b701e640b393bde2bbdbc40104cc537
Himadri Sekhar Basu <41947504+hsbasu@users.noreply.github.com>
2025-11-04 01:59:59 +0530

    Update copyright in all possible files (#76)
    - Update copyright year and message.

--- src/BatteryMonitor/main.py
+++ src/BatteryMonitor/main.py
@@ -1,24 +1,11 @@
-# Copyright (C) 2021-2024 Himadri Sekhar Basu <hsb10@iitbbs.ac.in>
-#
-# This file is part of battery-monitor.
-#
-# battery-monitor is free software: you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
-#
-# battery-monitor is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
-#
-# You should have received a copy of the GNU General Public License
-# along with battery-monitor. If not, see <http://www.gnu.org/licenses/>
-# or write to the Free Software Foundation, Inc., 51 Franklin Street,
-# Fifth Floor, Boston, MA 02110-1301, USA..
-#
-# Author: Himadri Sekhar Basu <hsb10@iitbbs.ac.in>
-#
+# -*- coding: utf-8 -*-
+# -----------------------------------------------------------------------------
+#  Battery Monitor – Maximize Your Battery Life.
+#  Copyright (C) 2016–2020 Maksudur Rahman Maateen
+#  Copyright (C) 2021–2025 Himadri Sekhar Basu
+#  Licensed under GPLv3 or later (see <http://www.gnu.org/licenses/>)
+# -----------------------------------------------------------------------------
+
 import gettext
 import locale
 import logging
